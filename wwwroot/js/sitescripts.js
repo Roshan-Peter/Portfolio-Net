f8d45c14f0e27f6386f0131fdd570e4d28533f1b
Roshan <roshanpeter26@hotmail.com>
2024-08-29 18:38:48 -0400

    small design changes

--- wwwroot/js/sitescripts.js
+++ wwwroot/js/sitescripts.js
@@ -7,6 +7,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
+    menuButton.classList.add('text-white');
+
     window.addEventListener('scroll', function () {
         var navElement = document.getElementById('mainNav');
         var scrollPosition = window.scrollY || document.documentElement.scrollTop;
@@ -16,11 +18,15 @@ document.addEventListener('DOMContentLoaded', function () {
             links.forEach(function (item) {
                 item.style.setProperty('color', 'white');
             });
+            menuButton.classList.remove('text-black');
+            menuButton.classList.add('text-white');
             navElement.classList.remove('bg-white');
         } else {
             links.forEach(function (item) {
-                item.style.setProperty('color', 'black'); 
+                item.style.setProperty('color', 'black');
             });
+            menuButton.classList.remove('text-white');
+            menuButton.classList.add('text-black');
             navElement.classList.add('bg-white');
         }
     });
